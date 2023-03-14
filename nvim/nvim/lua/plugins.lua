c2ddd9e49e41c16cc30229056e1de0abb9f2153b
Imtiyaz Zaman <imtiyaz.zaman@live.co.uk>
2023-03-14 20:26:32 +0000

    added config from neovim-for-beginners

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -29,9 +29,132 @@ function M.setup()
 
   local function plugins(use)
     use 'wbthomason/packer.nvim'
-    -- My plugins here
-    -- use 'foo1/bar1.nvim'
-    -- use 'foo2/bar2.nvim'
+
+    -- Load only when required
+    use { "nvim-lua/plenary.nvim", module = "plenary" }
+
+    -- Theme
+		use {
+			"sainnhe/everforest",
+			config = function()
+				vim.cmd "colorscheme everforest"
+			end,
+		}
+
+    -- Start screen
+    use {
+      'goolord/alpha-nvim',
+      requires = { 'nvim-tree/nvim-web-devicons' },
+      config = function ()
+        require'alpha'.setup(require'alpha.themes.startify'.config)
+      end
+		}
+
+    -- Git
+    use {
+      "TimUntersberger/neogit",
+			cmd = "Neogit",
+      requires = "nvim-lua/plenary.nvim",
+      config = function()
+        require("config.neogit").setup()
+      end,
+    }
+
+		-- WhichKey
+		use {
+			event = "VimEnter",
+			"folke/which-key.nvim",
+			config = function()
+				require("config.whichkey").setup()
+			end,
+		}
+
+    -- Better Comment
+    use {
+      "numToStr/Comment.nvim",
+      opt = true,
+      keys = { "gc", "gcc", "gbc" },
+      config = function()
+        require("Comment").setup {}
+      end,
+    }
+
+    -- Easy hopping
+    use {
+      "phaazon/hop.nvim",
+      cmd = { "HopWord", "HopChar1" },
+      config = function()
+        require("hop").setup {}
+      end,
+    }
+
+    -- Easy motion
+    use {
+      "ggandor/lightspeed.nvim",
+      keys = { "s", "S", "f", "F", "t", "T" },
+      config = function()
+        require("lightspeed").setup {}
+      end,
+    }
+		
+		-- Status Line
+		use {
+		  "nvim-lualine/lualine.nvim",
+			event = "VimEnter",
+			config = function()
+			 require("config.lualine").setup()
+			end,
+			requires = { "nvim-web-devicons" },
+		}
+		
+		-- Current code context
+		use {
+			"SmiteshP/nvim-gps",
+			requires = "nvim-treesitter/nvim-treesitter",
+			module = "nvim-gps",
+			config = function()
+				require("nvim-gps").setup()
+			end,
+		}
+
+		-- Treesitter
+		use {
+			"nvim-treesitter/nvim-treesitter",
+			 run = ":TSUpdate",
+			 config = function()
+				 require("config.treesitter").setup()
+			 end,
+		}
+
+		-- FZF
+		use { "junegunn/fzf", run = "./install --all" }
+		use { "junegunn/fzf.vim" }
+		use {
+		 "ibhagwan/fzf-lua",
+			requires = { "kyazdani42/nvim-web-devicons" },
+		}
+
+		-- File Explorer
+		use {
+		 "kyazdani42/nvim-tree.lua",
+		 requires = {
+			 "kyazdani42/nvim-web-devicons",
+		 },
+		 cmd = { "NvimTreeToggle", "NvimTreeClose" },
+			 config = function()
+				 require("config.nvimtree").setup()
+			 end,
+		}
+
+		-- Buffer line
+		use {
+			"akinsho/nvim-bufferline.lua",
+			event = "BufReadPre",
+			wants = "nvim-web-devicons",
+			config = function()
+				require("config.bufferline").setup()
+			end,
+		}
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
